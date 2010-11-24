984836ef0e448a37fa5e4c257da8715c1c28fc40
Colin Alstad <colin.alstad@gmail.com>
2010-11-24 00:12:55 -0500

    Added ability for extra patterns to be added by the user.

--- lib/readable_recurrences.rb
+++ lib/readable_recurrences.rb
@@ -6,14 +6,29 @@ end
 
 module ReadableRecurrences
   extend self
+  #Currently this library allows you determine the recurrence patterns
+  #weekly or monthly events. To definie your own pattern, the find
+  #method takes an optional extra_patterns array.
+  #Example:
+  # ReadableRecurrences.find(['2010-11-01', '2010-11-08', '2010-11-15'],
+  #                          {:default => false,
+  #                            :extra_patterns => [Pattern.new(:weeks => [1,2,3], :description => 'First Three']}
+  #
+  # Would return 'First Three Mondays in November 2010'
+
   DEFAULT_PATTERNS = [Pattern.new(:weeks => [1,2,3,4,5], :description => 'Every'),
                       Pattern.new(:weeks => [1,3], :description => 'First and Third'),
                       Pattern.new(:weeks => [2,4], :description => 'Second and Fourth'),
                      ]
+
   
-  def find(dates, default=true)
+  def find(dates, options={})
+    {:default => true}.merge!(options)
+    
     m = Matcher.new
-    m.patterns += DEFAULT_PATTERNS
+    m.patterns += DEFAULT_PATTERNS if options[:default]
+    m.patterns += options[:extra_patterns] if options[:extra_patterns]
+    
     m.find(dates)
   end
 end
